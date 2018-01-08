20b0b2911db8cb1f0734e48df57d1c8f87726fb0
kayo5994 <330956999@qq.com>
2018-01-08 14:52:32 +0800

    update: set 4 spaces indentation except Scss/CSS files and use the individual modules instead of gulp-util

--- generators/html/index.js
+++ generators/html/index.js
@@ -2,61 +2,62 @@
 var yeoman = require('yeoman-generator'),
     _ = require('lodash'),
     fs = require('fs'),
-    gutil = require('gulp-util');
+    log = require('fancy-log'),
+    colors = require('ansi-colors');
 
 module.exports = yeoman.Base.extend({
 
-  constructor: function () {
-    yeoman.Base.apply(this, arguments);
+    constructor: function () {
+        yeoman.Base.apply(this, arguments);
 
-    // fileName 为需要创建的 HTML 文件名
-    this.argument('fileName', {
-      type: String,
-      desc: 'HTML file name',
-      required: true
-    });
+        // fileName 为需要创建的 HTML 文件名
+        this.argument('fileName', {
+            type: String,
+            desc: 'HTML file name',
+            required: true
+        });
 
-    // include Options 表示输出 include header 和 footer 的模板而不是原生的 HTML 模板
-    this.option('include', {
-      desc: '输出 include header 和 footer 的模板',
-      type: Boolean
-    });
-  },
+        // include Options 表示输出 include header 和 footer 的模板而不是原生的 HTML 模板
+        this.option('include', {
+            desc: '输出 include header 和 footer 的模板',
+            type: Boolean
+        });
+    },
 
-  prompting: function () {
-    this.pageTitle = this.fileName;
+    prompting: function () {
+        this.pageTitle = this.fileName;
 
-    var prompts = [
-        {
-          type: 'input',
-          name: 'pageTitle',
-          message: '页面 Title:',
-          default: this.pageTitle
+        var prompts = [
+            {
+                type: 'input',
+                name: 'pageTitle',
+                message: '页面 Title:',
+                default: this.pageTitle
+            }
+        ];
+
+        return this.prompt(prompts).then(function (props) {
+            this.pageTitle = props.pageTitle;
+        }.bind(this));
+    },
+
+    writing: function () {
+
+        // 生成 HTML 文件
+        var readmeHeaderTmpl = null;
+        if (this.options.include) {
+            readmeHeaderTmpl = _.template(this.fs.read(this.templatePath('include.html')));
+        } else {
+            readmeHeaderTmpl = _.template(this.fs.read(this.templatePath('template.html')));
         }
-    ];
 
-    return this.prompt(prompts).then(function (props) {
-      this.pageTitle = props.pageTitle;
-    }.bind(this));
-  },
+        fs.writeFileSync(this.destinationPath(this.fileName + '.html'), readmeHeaderTmpl({
+            pageTitle: this.pageTitle
+        }));
 
-  writing: function () {
+        log(colors.green('QMUI Generator: ') + '创建新 HTML 文件 ' + this.fileName + '.html');
 
-    // 生成 HTML 文件
-    var readmeHeaderTmpl = null;
-    if (this.options.include) {
-      readmeHeaderTmpl = _.template(this.fs.read(this.templatePath('include.html')));
-    } else {
-      readmeHeaderTmpl = _.template(this.fs.read(this.templatePath('template.html')));
+        process.exit(0);
     }
-
-    fs.writeFileSync(this.destinationPath(this.fileName + '.html'), readmeHeaderTmpl({
-      pageTitle: this.pageTitle
-    }));
-
-    gutil.log(gutil.colors.green('QMUI Generator: ') + '创建新 HTML 文件 ' + this.fileName + '.html');
-
-    process.exit(0);
-  }
 
 });
--- generators/scss/index.js
+++ generators/scss/index.js
@@ -4,51 +4,52 @@ var yeoman = require('yeoman-generator'),
     fs = require('fs'),
     os = require('os'),
     path = require('path'),
-    gutil = require('gulp-util');
+    log = require('fancy-log'),
+    colors = require('ansi-colors');
 
 module.exports = yeoman.Base.extend({
 
-  constructor: function () {
-    yeoman.Base.apply(this, arguments);
+    constructor: function () {
+        yeoman.Base.apply(this, arguments);
 
-    // fileName 为需要创建的 Scss 文件名
-    this.argument('fileName', {
-      type: String,
-      desc: 'scss file name',
-      required: true
-    });
-  },
+        // fileName 为需要创建的 Scss 文件名
+        this.argument('fileName', {
+            type: String,
+            desc: 'scss file name',
+            required: true
+        });
+    },
 
-  writing: function () {
+    writing: function () {
 
-    // 检查时间格式（自动补0）
-    var checkDateFormat = function(date) {
-      var result = date;
-      if (result < 10) {
-        result = '0' + result;
-      }
+        // 检查时间格式（自动补0）
+        var checkDateFormat = function (date) {
+            var result = date;
+            if (result < 10) {
+                result = '0' + result;
+            }
 
-      return result;
-    };
+            return result;
+        };
 
-    // 获取当前时间
-    var currentDate = new Date(),
-        currentYear = currentDate.getFullYear(),
-        currentMonth = checkDateFormat(currentDate.getMonth() + 1),
-        currentDay = checkDateFormat(currentDate.getDate()),
-        formattingDate = currentYear + '-' + currentMonth + '-' + currentDay;
+        // 获取当前时间
+        var currentDate = new Date(),
+            currentYear = currentDate.getFullYear(),
+            currentMonth = checkDateFormat(currentDate.getMonth() + 1),
+            currentDay = checkDateFormat(currentDate.getDate()),
+            formattingDate = currentYear + '-' + currentMonth + '-' + currentDay;
 
-    // 生成 Scss 文件
-    var readmeHeaderTmpl = _.template(this.fs.read(this.templatePath('template.scss')));
-    fs.writeFileSync(this.destinationPath(this.fileName + '.scss'), readmeHeaderTmpl({
-      fileName: this.fileName,
-      author: _.upperFirst(path.basename(os.homedir())),
-      createDate: formattingDate
-    }));
+        // 生成 Scss 文件
+        var readmeHeaderTmpl = _.template(this.fs.read(this.templatePath('template.scss')));
+        fs.writeFileSync(this.destinationPath(this.fileName + '.scss'), readmeHeaderTmpl({
+            fileName: this.fileName,
+            author: _.upperFirst(path.basename(os.homedir())),
+            createDate: formattingDate
+        }));
 
-    gutil.log(gutil.colors.green('QMUI Generator: ') + '创建新 Scss 文件 ' + this.fileName + '.scss');
+        log(colors.green('QMUI Generator: ') + '创建新 Scss 文件 ' + this.fileName + '.scss');
 
-    process.exit(0);
-  }
+        process.exit(0);
+    }
 
 });
